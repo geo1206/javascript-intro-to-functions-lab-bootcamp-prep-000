b996b8986190768e8e9de4f8af33b2d849169c36
energetic-gateway-3088 <geobannyborja@gmail.com>
2020-10-07 04:08:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return ("I can't hear you!") if (string===string.toLowerCase())
+  if(string===string.toLowerCase()) return ("I can't hear you!") 
 }
